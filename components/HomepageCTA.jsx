18231ef6c44d60d7b1b05df922517c4c29ddf1af
Nikola Štefančić <nikola.stefancic@gmail.com>
2026-05-06 09:42:53 +0200

    feat: update HomepageCTA and Testimonials components for improved styling and layout, and bump version to 0.1.3

--- components/HomepageCTA.jsx
+++ components/HomepageCTA.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const HomepageCTA = () => {
   return (
-    <section className="bg-[#1E293B] py-16 px-12">
+    <section className="bg-[#1E293B] py-24 px-12">
       <div className="max-w-7xl mx-auto flex flex-col items-center justify-center gap-5">
         <h2 className="text-4xl font-bold text-white text-center">
           Ready to find your contractor?
@@ -16,8 +16,8 @@ const HomepageCTA = () => {
         </Link>
 
         <p className="text-[#94A3B8] text-sm align-middle">
-          Or{" "}
-          //TODO - Change the link to the contractor registration page once it's implemented
+          Or //TODO - Change the link to the contractor registration page once
+          it's implemented
           <Link
             href="/register?role=contractor"
             className="underline hover:text-white transition-colors"
--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,26 +1,27 @@
 import connectDB from "@/config/database";
 import Review from "@/models/Review";
 import TestimonialsCard from "./TestimonialsCard";
-
+import User from "@/models/User";
 
 const Testimonials = async () => {
   await connectDB();
 
-  const reviews = await Review.find({ verified: true }).populate("user", "name profileImage").lean();
-  
+  const reviews = await Review.find({ verified: true })
+    .populate("user", "name profileImage")
+    .lean();
+
   // Filter out reviews with null user references (orphaned data)
   const validReviews = reviews.filter((review) => review.user !== null);
-  
-    
+
   return validReviews.length > 0 ? (
-    <section className="bg-white px-4 py-5 pb-10">
+    <section className="bg-white px-4 py-6 pb-16">
       <div className="w-full container-xl lg:container m-auto">
         <h2 className="text-3xl font-extrabold text-[#071525] text-center my-4 ">
           Trusted by Homeowners
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8 max-w-6xl mx-auto ">
           {validReviews.map((review) => (
-            <TestimonialsCard key={review._id} review={review} /> 
+            <TestimonialsCard key={review._id} review={review} />
           ))}
         </div>
       </div>
